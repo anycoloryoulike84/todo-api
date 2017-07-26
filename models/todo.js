788ae51062f14e797a1f3d5f9128e2d1326efcbb
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-07-25 20:50:47 -0700

    images working, search filter working, modified schema

--- models/todo.js
+++ models/todo.js
@@ -15,6 +15,8 @@ module.exports = mongoose.model('todo', TodoSchema);
 
 // curl -XPOST http://localhost:3000/todos -d 'name=London%20Pride&happyHour=true&beerType=Ale&description=Classic%20British%20Ale&brewery=Fullers&cost=6.5'
 
-// curl -XPUT http://localhost:3000/todos/5977da900fb2e635f6fe89c4 -d 'description=Classic%20Irish%20Stout'
 
-// curl -X "DELETE" http://localhost:3000/todos/5977db17afaa54360f8abafe
+
+// curl -XPUT http://localhost:3000/todos/59780cf1e5c3e03b11c364bb -d 'description=http%3A%2F%2F21st-amendment.com%2Fassets%2Fbrewfree_can_022113-230x409.png'
+
+// curl -X "DELETE" http://localhost:3000/todos/5977fb8e21aab138f5feeb4f
